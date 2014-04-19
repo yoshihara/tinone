5cae39bfbe045f185d7e77c153018b158f3860e9
yoshihara <h.yoshihara@everyleaf.com>
2014-04-20 00:34:32 +0900

    Add already implementation

---
+++ tinone.js
@@ -0,0 +1,139 @@
+﻿var mainCtrl = function($scope, $http) {
+    var ls = localStorage;
+
+    var syncFromLocalStorage = function() {
+        var tasks = JSON.parse(ls.getItem("items"));
+        $scope.tasks = tasks;
+    }
+
+    var syncToLocalStorage = function() {
+        ls.setItem("items", JSON.stringify($scope.tasks));
+    }
+
+    $scope.tasks = [];
+    if(ls.getItem("items") == null) ls.setItem("items", JSON.stringify([]));
+    syncFromLocalStorage();
+
+    $scope.addNew = function() {
+        var newTask = {
+            "body":$scope.newTaskBody,
+            "done":false,
+            "startTime":undefined,
+            "endTime":undefined,
+            "clockStatus":"",
+            "elapsed":0
+        };
+        $scope.tasks.push(newTask);
+        syncToLocalStorage();
+        $scope.newTaskBody = "";
+    }
+
+    $scope.startClock = function(index) {
+        $scope.tasks[index].startTime = Date.now();
+        $scope.tasks[index].clockStatus = "計測中...";
+        syncToLocalStorage();
+    }
+
+    $scope.endClock = function(index) {
+        var task = $scope.tasks[index]
+        task.endTime = Date.now();
+        if(task.elapsed == undefined) task.elapsed = 0;
+        task.elapsed += (task.endTime - task.startTime) / 1000 / 60;
+        task.clockStatus = "";
+        syncToLocalStorage();
+    }
+
+    $scope.doneTask = function(index) {
+        var task = $scope.tasks[index]
+        task.done = !task.done; // TODO: checkedを見たほうがいい
+        if(task.done == true) {
+            task.clockStatus = "終了";
+            task.endTime = Date.now();
+        } else {
+            task.clockStatus = "";
+        }
+        syncToLocalStorage();
+    }
+
+    $scope.deleteTask = function(index) {
+        if(confirm("削除しますか？")) {
+            $scope.tasks.splice(index,1);
+            syncToLocalStorage();
+        }
+    }
+
+    $scope.getDoneCount = function() {
+        var count = 0;
+        angular.forEach($scope.tasks, function(task) {
+            count += task.done ? 1 : 0;
+        });
+        return count;
+    }
+
+    $scope.deleteDone = function() {
+        if(confirm("終了済みのタスクを削除しますか？")) {
+            var oldTasks = $scope.tasks;
+            $scope.tasks = [];
+            angular.forEach(oldTasks, function(task) {
+                if (!task.done) $scope.tasks.push(task);
+            });
+            syncToLocalStorage();
+        };
+    }
+
+    $scope.getDoneTasks = function() {
+        var doneTasks = [];
+        angular.forEach($scope.tasks, function(task) {
+            if (task.done) doneTasks.push(task);
+        });
+        return doneTasks;
+    }
+
+    $scope.doneDate = function(doneTask) {
+        if (doneTask.endTime == undefined) return "";
+        var doneDate = new Date(doneTask.endTime);
+        return doneDate.getFullYear() + "/" + (doneDate.getMonth() + 1) + "/" + doneDate.getDate();
+    }
+
+    $scope.exportCSV = function() {
+        var doneTasks = $scope.getDoneTasks();
+        var csv = "date,body,elapsed(min)\n";
+
+        angular.forEach(doneTasks, function(task) {
+            csv = csv + $scope.doneDate(task) + ',"' + task.body + '",' + task.elapsed + "\n"
+        });
+        return csv;
+    }
+}
+
+
+// (function() {
+//     if (typeof(Storage) === "undefined") {
+//  alert("Web Strage Not Supported");
+//     } else {
+//  var ls = localStorage;
+//  // ls.setItem('name', 'taguchi');
+//  // console.log(ls.getItem("name"));
+//  // ls.removeItem('taguchi');
+//  // ls.clear();
+//  // s.length;　括弧いらない
+//  // s.key(i), s.getItem(s.key(i))
+
+//  var user = {
+//      name: 'taguchi',
+//      score: 80
+//  };
+
+//  // ls.setItem('user', JSON.stringify(user));
+//  // console.dir(JSON.parse(ls.getItem('user')))
+
+//  window.addEventListener('storage', function(e) {
+//      // e.key
+//      // e.oldValue, e.newValue
+//      // e.storageArea
+//      // e.url
+//      alert("aaa");
+//      console.log(e.key + ":" + e.oldValue + "→" + e.newValue);
+//  });
+//     }
+// })();
