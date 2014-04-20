4b9b3ee0b95b3881a75606a3e18ebaaca5179ac7
yoshihara <h.yoshihara@everyleaf.com>
2014-04-20 17:52:31 +0900

    spec: Add taskStorage spec [WIP]
    TODO: add more spec

--- spec/tinone.spec.js
+++ spec/tinone.spec.js
@@ -2,6 +2,10 @@ describe("Task", function() {
   it("should be defined", function () {
     expect(Task).toBeDefined();
   });
+});
 
-
+describe("taskStorage", function() {
+  it("should be defined", function () {
+    expect(taskStorage).toBeDefined();
+  });
 });
