a4567a0d8c37c9877de30a0bf7ef2214bb7cf282
yoshihara <h.yoshihara@everyleaf.com>
2014-04-20 09:55:36 +0900

    js: Add missing semicolon

--- tinone.js
+++ tinone.js
@@ -4,11 +4,11 @@
     var syncFromLocalStorage = function() {
         var tasks = JSON.parse(ls.getItem("items"));
         $scope.tasks = tasks;
-    }
+    };
 
     var syncToLocalStorage = function() {
         ls.setItem("items", JSON.stringify($scope.tasks));
-    }
+    };
 
     $scope.tasks = [];
     if(ls.getItem("items") == null) ls.setItem("items", JSON.stringify([]));
@@ -26,13 +26,13 @@
         $scope.tasks.push(newTask);
         syncToLocalStorage();
         $scope.newTaskBody = "";
-    }
+    };
 
     $scope.startClock = function(index) {
         $scope.tasks[index].startTime = Date.now();
         $scope.tasks[index].clockStatus = "計測中...";
         syncToLocalStorage();
-    }
+    };
 
     $scope.endClock = function(index) {
         var task = $scope.tasks[index]
@@ -41,7 +41,7 @@
         task.elapsed += (task.endTime - task.startTime) / 1000 / 60;
         task.clockStatus = "";
         syncToLocalStorage();
-    }
+    };
 
     $scope.doneTask = function(index) {
         var task = $scope.tasks[index]
@@ -53,14 +53,14 @@
             task.clockStatus = "";
         }
         syncToLocalStorage();
-    }
+    };
 
     $scope.deleteTask = function(index) {
         if(confirm("削除しますか？")) {
             $scope.tasks.splice(index,1);
             syncToLocalStorage();
         }
-    }
+    };
 
     $scope.getDoneCount = function() {
         var count = 0;
@@ -68,7 +68,7 @@
             count += task.done ? 1 : 0;
         });
         return count;
-    }
+    };
 
     $scope.deleteDone = function() {
         if(confirm("終了済みのタスクを削除しますか？")) {
@@ -79,7 +79,7 @@
             });
             syncToLocalStorage();
         };
-    }
+    };
 
     $scope.getDoneTasks = function() {
         var doneTasks = [];
@@ -87,13 +87,13 @@
             if (task.done) doneTasks.push(task);
         });
         return doneTasks;
-    }
+    };
 
     $scope.doneDate = function(doneTask) {
         if (doneTask.endTime == undefined) return "";
         var doneDate = new Date(doneTask.endTime);
         return doneDate.getFullYear() + "/" + (doneDate.getMonth() + 1) + "/" + doneDate.getDate();
-    }
+    };
 
     $scope.exportCSV = function() {
         var doneTasks = $scope.getDoneTasks();
@@ -103,8 +103,8 @@
             csv = csv + $scope.doneDate(task) + ',"' + task.body + '",' + task.elapsed + "\n"
         });
         return csv;
-    }
-}
+    };
+};
 
 
 // (function() {
